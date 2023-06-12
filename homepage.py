8d5f7310014a45e4a87ba4099a3816f21fb8cb3c
Zim <abrormhazim@gmail.com>
2023-06-12 10:19:51 +0700

    edit nama sidebar

--- homepage.py
+++ homepage.py
@@ -7,7 +7,7 @@ st.set_page_config(
 
 st.title("Tugas Besar Mata Kuliah Perancangan Aplikasi Sains Data")
 st.sidebar.success("Select a page above.")
-st.info("Click on the left sidebar menu to navigate to the different apps.")
+st.info("Click on the left sidebar menu to navigate to the different pages.")
 
 st.header("Kelompok 4")
 st.header("Anggota Kelompok:")

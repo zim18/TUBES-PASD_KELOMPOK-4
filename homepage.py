0608253e9ba6457530442d30bc12bb1eec52ba9f
Zim <abrormhazim@gmail.com>
2023-06-12 10:16:43 +0700

    edit nama kelompok

--- homepage.py
+++ homepage.py
@@ -8,3 +8,9 @@ st.set_page_config(
 st.title("Tugas Besar Mata Kuliah Perancangan Aplikasi Sains Data")
 st.sidebar.success("Select a page above.")
 st.info("Click on the left sidebar menu to navigate to the different apps.")
+
+st.header("Kelompok 4")
+st.header("Anggota Kelompok:")
+st.markdown("1. Abror Muhammad Hazim (1305213026)")
+st.markdown("2. Cetrin Azahra (1305210072)")
+st.markdown("3. Ikhwan Wahyudin (1305210065)")
